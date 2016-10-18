94372b4a1aa4eaa5b03c4d39fa925b844df45b46
Judson James <judsonjames96@gmail.com>
2016-10-18 12:14:13 -0400

    Assets
    Assets (Updated @ 12:13PM 10-18-16)

---
+++ Team%20M.E.M.E/Assets/Scripts/JudsonsCollisionCode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+/******************************************************************************
+ * Author: Judson James
+ * Date: 10-17-16
+ * Purpose: Mostly to make the movements look natural
+**/
+public class JudsonsCollisionCode : MonoBehaviour {
+
+	// void Start() : Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// void Update() : Update is called once per frame
+	void Update () {
+       // if (GameObject.FindWithTag("Barrier").
+       //     .IsTouching(GameObject.FindGameObjectsWithTag("Barrier")));
+	}
+}
