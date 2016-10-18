c4a7bb548f02ae8a3572366de558e24e1af6494b
banangineer <judsonjames96@gmail.com>
2016-10-18 12:24:33 -0400

    Updated Files 10-18-16
    Had to remove all old files; files have been reuploaded to match current
    progress.

---
+++ Assets/Scripts/MovePosition.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+/******************************************************************************
+ * Author: Judson James
+ * Date Started: 10/16/16
+ * Purpose: General Movement Code for Test Scene
+**/
+public class MovePosition : MonoBehaviour {
+    // Instance Variables
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    // void Start() : Use this for initialization *****************************
+    void Start () {
+        
+	}
+
+    // void Update() : Update is called once per frame ************************
+    void Update () {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 
+                                        Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump")) {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+/******************************************************************************
+ * Author: Judson James
+ * Date: 10-17-16
+ * Purpose: Stop ALL rotation.
+**/
+public class Rotation : MonoBehaviour {
+
+    // Instance Variables
+    public bool rotationAllToggle = false;
+    public bool rotationXToggle = false;
+    public bool rotationYToggle = false;
+	
+    // void Start() : Use this for initialization
+	void Start() {
+	
+	}
+	
+	// void Update() : Update is called once per frame
+	void Update() {
+	    if (rotationAllToggle == true) {
+            GetComponent<Rigidbody2D>().freezeRotation = true;
+        }
+        if (rotationXToggle == true) {
+            GetComponent<Rigidbody2D>().freezeRotation = true;
+        }
+        if (rotationYToggle == true) {
+            GetComponent<Rigidbody2D>().freezeRotation = true;
+        }
+	}
+}
