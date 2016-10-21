ca3f2b840f5958c761b3456fb54664fbcce7f716
Joshua Bush <Hazgam44@gmail.com>
2016-10-20 23:00:07 -0400

    JumpChange
    Changed to jump to an Addforce rather than a translate

--- Assets/Scripts/BoolMove.cs
+++ Assets/Scripts/BoolMove.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 /******************************************************************************
- * Author: Judson James
+ * Author: Judson James, Joshua Bush
  * Date Started: 10/16/16
  * Purpose: General Movement Code for Test Scene
+ * Date changed: 10/20/16
+ * Purpose: Changing jump to and Add force instead of translate
 **/
 
 public class BoolMove : MonoBehaviour {
@@ -11,8 +13,11 @@ public class BoolMove : MonoBehaviour {
     public int horizontalChange = 0;
     public int jumpHeight = 0;
     public float speed = 0;
+    public Rigidbody2D rb;
+    public float jumpForce = 1.0f;
     // void Start() : Use this for initialization *****************************
     void Start () {
+        rb = GetComponent<Rigidbody2D>();
 	
 	}
 
@@ -20,16 +25,17 @@ public class BoolMove : MonoBehaviour {
     void Update () {
 
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        /*float vertical = Input.GetAxis("Vertical");
         if (vertical < 0)
         {
             vertical = 0;
         }
+        */
         horizontal = horizontal * Time.deltaTime * horizontalChange * speed;
-        vertical = vertical * Time.deltaTime * jumpHeight * speed;
+       // vertical = vertical * Time.deltaTime * jumpHeight * speed;
         transform.Translate(horizontal, 0, 0);
-        transform.Translate(0, vertical, 0);
-        
+        //transform.Translate(0, vertical, 0);
+
         /*
         // Movement from directional keys
         if (Input.GetKeyDown(KeyCode.A))
@@ -44,12 +50,16 @@ public class BoolMove : MonoBehaviour {
             position.x = position.x + horizontalChange; // Right
             this.transform.position = position;
         }
+        */
         if (Input.GetKeyDown(KeyCode.Space))
         {
+                rb.AddForce(transform.up * jumpForce);
+
+            /*
             Vector3 position = this.transform.position;
-            position.y = position.y + jumpHeight; // Up
+            position.y = position.y*Time.deltaTime; // Up
             this.transform.position = position;
+            */
         }
-        */
     }
 }
